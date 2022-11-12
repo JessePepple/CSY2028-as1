c6d361dfd0d01220cf3f57cef05d9d1f46ce4c7c
Pluto101** <jessepepple36@gmail.com>
2022-11-12 10:40:55 +0100

    Added category and auction tables

--- websites/as1/database/import.sql
+++ websites/as1/database/import.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS users(id int not null auto_increment, name varchar(40
 -- password: 123456 
 INSERT IGNORE INTO users(id, name, email, password, is_admin)
 VALUES('1', 'admin', 'admin@test.com', '$2y$10$4gUDYfIyHBDsiqdUy90MUu.IJvQYs3kXS7Xk/lHLG3Bn/.h7oln4m', '1');
+
+CREATE TABLE if not exists category(id INT NOT null auto_increment, name varchar(60) not null, primary key(id));
+
+insert ignore into category(id, name) values('1', 'Electronics');
+insert ignore into category(id, name) values('2', 'Home and garden');
+insert ignore into category(id, name) values('3', 'Fashion');
+insert ignore into category(id, name) values('4', 'Sport');
+
+CREATE TABLE if not exists auction(id int not null auto_increment, title varchar(255) not null, description text not null,
+categoryId int not null, endDate date not null, foreign key(categoryId) references category(id), primary key(id));
