11c93a01824703ad53ed6423b77e4e42d3fc735c
Pluto101** <jessepepple36@gmail.com>
2022-11-15 06:43:05 +0100

    Users can now place bids

--- websites/as1/public/auction.php
+++ websites/as1/public/auction.php
@@ -21,10 +21,68 @@ if(!$auction)
 
 $author = get_user($auction['user_id'], $db);
 
+/**
+ * Creates a review
+ * @param array $data
+ * @param $db
+ */
+function create_review(array $data, $db)
+{
+    if(!isset($data['user_id']) || !isset($data['reviewer_id']) || !isset($data['review_text'])) 
+    return false;
+
+    $sql = "INSERT INTO reviews(user_id, reviewer_id, review_text) VALUES(?, ?, ?)";
+    $query = $db->prepare($sql);
+    
+    return $query->execute([$data['user_id'], $data['reviewer_id'], $data['review_text']]);
+}
+
+/**
+ * Places a bid
+ * @param array $data
+ * @param $db
+ */
+function place_bid(array $data, $db)
+{
+    if(!isset($data['user_id']) || empty($data['amount']) || !isset($data['auction_id']) || !is_numeric($data['amount']))
+    return false;
+
+    $sql = "INSERT INTO bids(user_id, auction_id, amount) VALUES(?, ?, ?)";
+    $query = $db->prepare($sql);
+
+    return $query->execute([$data['user_id'], $data['auction_id'], $data['amount']]);
+}
+
+// when bid form is submitted
+if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['bid_form']) && isset($_SESSION['id']))
+{
+    $bid_data = [
+        'auction_id' => $auction['id'],
+        'user_id' => $_SESSION['id'],
+        'amount' => $_POST['bid']
+    ];
+
+    if(!place_bid($bid_data, $db))
+    {
+        $error_msg = 'Please use a valid bid amount';
+    }
+    else
+    {
+        $success_msg = 'Your bid has been placed successfully';
+    }
+
+}
 include_once('header.php');
 ?>
 
 <h1>Product Page</h1>
+<?php if(isset($success_msg)): ?>
+    <div style="color:green; padding: 10px 20px"><?= $success_msg ?></div>
+<?php endif ?>
+
+<?php if(isset($error_msg)): ?>
+    <div style="color:red; padding: 10px 20px"><?= $error_msg ?></div>
+<?php endif ?>
 <article class="product">
 
     <img src="product.png" alt="product name">
@@ -36,8 +94,9 @@ include_once('header.php');
         <time>Time left: <?= end_date($auction['endDate']) ?></time>
 
         <?php if(isset($_SESSION['id'])): ?>
-        <form action="" class="bid" method="post" name="bid_form">
+        <form action="" class="bid" method="post">
             <input type="text" name="bid" placeholder="Enter bid amount" />
+            <input type="hidden" name="bid_form" />
             <input type="submit" value="Place bid" />
         </form>
         <?php endif ?>
